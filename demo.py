d50276593d45c45df354997e5956e624adf5878c
David Buchanan <d@vidbuchanan.co.uk>
2024-12-06 15:59:28 +0000

    initial commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+from magic_numbers import FORTY_TWO, SIXTY_NINE, FOUR_HUNDRED_AND_TWENTY
+from magic_numbers import ONE_THOUSAND_THREE_HUNDRED_AND_TWELVE
+
+print(f"{FORTY_TWO = }")
+print(f"{SIXTY_NINE = }")
+print(f"{FOUR_HUNDRED_AND_TWENTY = }")
+print(f"{ONE_THOUSAND_THREE_HUNDRED_AND_TWELVE = }")
