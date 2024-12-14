5c0f93393d0b904ef411147a9ea9ffe792fbd057
Alec Nunn <alec.nunn@gmail.com>
2024-12-13 18:19:30 -0800

    Adding basic float support

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ Magically import magic number constants. Like so:
 ```py
 from magic_numbers import FORTY_TWO, SIXTY_NINE, FOUR_HUNDRED_AND_TWENTY
 from magic_numbers import ONE_THOUSAND_THREE_HUNDRED_AND_TWELVE
+from magic_numbers import THREE_POINT_ONE_FOUR_ONE_FIVE_NINE_TWO
 
 print(f"{FORTY_TWO = }")  # 42
 print(f"{SIXTY_NINE = }")  # 69
 print(f"{FOUR_HUNDRED_AND_TWENTY = }")  # 420
 print(f"{ONE_THOUSAND_THREE_HUNDRED_AND_TWELVE = }")  # 1312
+print(f"{THREE_POINT_ONE_FOUR_ONE_FIVE_NINE_TWO = }")  # 3.141592
 ```
 
 ## Installation
--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
 from magic_numbers import FORTY_TWO, SIXTY_NINE, FOUR_HUNDRED_AND_TWENTY
 from magic_numbers import ONE_THOUSAND_THREE_HUNDRED_AND_TWELVE
 from magic_numbers import FOUR_CENTILLION_SIX_SEPTENONAGINTILLION
+from magic_numbers import THREE_POINT_ONE_FOUR_ONE_FIVE_NINE_TWO
 
 print(f"{FORTY_TWO = }")
 print(f"{SIXTY_NINE = }")
 print(f"{FOUR_HUNDRED_AND_TWENTY = }")
 print(f"{ONE_THOUSAND_THREE_HUNDRED_AND_TWELVE = }")
 print(f"{FOUR_CENTILLION_SIX_SEPTENONAGINTILLION = }")
+print(f"{THREE_POINT_ONE_FOUR_ONE_FIVE_NINE_TWO = }")
 
 try:
 	from magic_numbers import NOT_A_NUMBER
